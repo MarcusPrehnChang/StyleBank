5501cd6b9a678907e2dbe419e08108ae1e4aa47a
MarcusPrehnChang <s224294@student.dtu.dk>
2023-11-19 15:15:03 +0100

    Changes to viewmodel, and also added ObservableList and ObservableListObserver, we still need to test it, which is the next step.

---
+++ app/src/main/java/com/example/stylebank/model/ObservableList.kt
@@ -0,0 +1,30 @@
+package com.example.stylebank.model
+
+import android.database.Observable
+
+class ObservableList<T> : ArrayList<T>() {
+    private val observers = mutableListOf<ObservableListObserver<T>>()
+
+    fun registerObserver(observer : ObservableListObserver<T>){
+        observers.add(observer)
+    }
+
+    fun removeObserver(observer : ObservableListObserver<T>){
+        observers.remove(observer)
+    }
+
+    override fun add(element: T): Boolean{
+        val result = super.add(element)
+        if(result){
+            notifyItemAdded(element)
+        }
+        return result
+    }
+
+    private fun notifyItemAdded(item : T){
+        observers.forEach{
+            it.onItemAdded(item)
+        }
+    }
+
+}
--- app/src/main/java/com/example/stylebank/viewmodel/ProductViewModel.kt
+++ app/src/main/java/com/example/stylebank/viewmodel/ProductViewModel.kt
@@ -1,10 +1,18 @@
 package com.example.stylebank.viewmodel
 
+import androidx.lifecycle.MutableLiveData
+import com.example.stylebank.data.ClothingRepository
 import com.example.stylebank.model.Banner
 import com.example.stylebank.model.Clothing
+import com.example.stylebank.model.ObservableList
 
-class ProductViewModel { //List to listen to, using some kind of observable pattern, i believe Kotlin has a unique one that is best to use.
-    private val productList = ArrayList<Clothing>()
-    private val banners = ArrayList<Banner>()
-    private val likedItems = ArrayList<Clothing>()
+class ProductViewModel(private val repository: ClothingRepository) { //List to listen to, using some kind of observable pattern, i believe Kotlin has a unique one that is best to use.
+    private val listsMap: Map<String, ObservableList<*>> = mapOf(
+        "product" to ObservableList<Clothing>(),
+        "banner" to ObservableList<Banner>(),
+        "likedItem" to ObservableList<Clothing>()
+    )
+    fun getList(key: String): ObservableList<Any>?{
+        return listsMap[key] as? ObservableList<Any>
+    }
 }
