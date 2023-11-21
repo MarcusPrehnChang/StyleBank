84a86d3419dc20fe51a5e6d4724d6711e8279775
Nicolai Rongsted <Nicolai.Rongsted@gmail.com>
2023-11-21 16:58:12 +0100

    99% finished info popup screen

--- app/src/main/java/com/example/stylebank/ui/theme/SwipeActivity.kt
+++ app/src/main/java/com/example/stylebank/ui/theme/SwipeActivity.kt
@@ -18,25 +18,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredHeight
-import androidx.compose.foundation.layout.requiredSize
-import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.stylebank.R
-import com.example.stylebank.ui.theme.SwipeScreen
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.getValue
@@ -44,13 +34,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.input.pointer.positionChange
@@ -213,24 +200,7 @@ fun SwipeScreen() {
                     modifier = Modifier.align(Alignment.Center)
                 )
             }
-        if (isOverlayVisible) {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color.White)
-                    .padding(16.dp)
-            ) {
-                Text(
-                    text = "Your Overlay Content Here",
-                    style = TextStyle(
-                        color = Color.Black,
-                        fontSize = 18.sp,
-                        fontWeight = FontWeight.Bold,
-                        fontFamily = FontFamily.SansSerif
-                    )
-                )
-            }
-        }
+
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -250,55 +220,238 @@ fun SwipeScreen() {
                     horizontalArrangement = Arrangement.SpaceEvenly,
                     verticalAlignment = Alignment.CenterVertically
                 ) {
-                    IconButtonWithText(
+                    MenuBarButton(
                         onClick = { /* handle button click */ },
                         icon = painterResource(id = R.drawable.icon_ild),
                     )
 
-                    IconButtonWithText(
+                    MenuBarButton(
                         onClick = { /* handle button click */ },
                         icon = painterResource(id = R.drawable.icon_swipe),
                         iconSize = 38.dp
                     )
 
-                    IconButtonWithText(
+                    MenuBarButton(
                         onClick = { /* handle button click */ },
                         icon = painterResource(id = R.drawable.icon_mb),
                     )
                 }
             }
         }
+    if (isOverlayVisible) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White)
+                .padding(16.dp)
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.Start
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight(0.5f)
+                        .padding(start = 16.dp, top = 0.dp, end = 16.dp, bottom = 16.dp)
+                        .background(color = Color.Gray, shape = RoundedCornerShape(40.dp))
+                )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.Bottom
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .height(180.dp)
+                            .width(180.dp)
+                            .padding(start = 16.dp, top = 0.dp, end = 0.dp, bottom = 0.dp)
+                            .background(color = Color.Gray, shape = RoundedCornerShape(40.dp))
+                    )
+                    Box(
+                        modifier = Modifier
+                            .height(180.dp)
+                            .width(180.dp)
+                            .padding(start = 0.dp, top = 0.dp, end = 16.dp, bottom = 0.dp)
+                            .background(color = Color.Gray, shape = RoundedCornerShape(40.dp))
+                    )
 
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.Bottom
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .height(100.dp)
+                            .fillMaxWidth()
+                            .padding(16.dp)
+                            .background(color = Color.Black, shape = RoundedCornerShape(10.dp))
+                    ) {
+                        Text(
+                            text = "STORE",
+                            color = Color.White,
+                            style = TextStyle(
+                                fontSize = 25.sp,
+                                fontWeight = FontWeight.Bold,
+                                fontFamily = FontFamily.SansSerif
+                            ),
+                            modifier = Modifier.align(Alignment.Center)
+                        )
+                        Image(
+                            painter = painterResource(id = R.drawable.store),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .size(50.dp)
+                                .align(Alignment.CenterEnd)
+                                .padding(end = 16.dp)
+                        )
+                    }
+                }
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = "CAMO SHIRT",
+                            style = TextStyle(
+                                fontFamily = FontFamily.SansSerif,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 20.sp
+                            ),
+                            color = Color.Black
+                        )
+                        Box(
+                            modifier = Modifier
+                                .height(25.dp)
+                                .width(65.dp)
+                                .background(
+                                    color = PriceTagGreen.copy(alpha = 0.5f),
+                                    shape = RoundedCornerShape(8.dp)
+                                )
+                        ) {
+                            Text(
+                                text = "118£",
+                                color = Color.Black,
+                                style = TextStyle(
+                                    fontSize = 20.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    fontFamily = FontFamily.SansSerif
+                                ),
+                                modifier = Modifier.align(Alignment.Center)
+                            )
+                        }
+                    }
+                    Text(
+                        text = "SAUNA",
+                        style = TextStyle(
+                            fontFamily = FontFamily.SansSerif,
+                            fontWeight = FontWeight.SemiBold,
+                            fontSize = 18.sp
+                        ),
+                        color = Color.Gray
+                    )
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.Bottom,
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .height(100.dp)
+                                .width(250.dp)
+                                .padding(start = 0.dp, top = 8.dp, end = 0.dp, bottom = 0.dp)
+                                .background(color = BankBlue, shape = RoundedCornerShape(10.dp)),
+                        ) {
+                            Text(
+                                text = "BANK",
+                                color = Color.White,
+                                style = TextStyle(
+                                    fontSize = 25.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    fontFamily = FontFamily.SansSerif
+                                ),
+                                modifier = Modifier.align(Alignment.Center)
+                            )
+                            Image(
+                                painter = painterResource(id = R.drawable.bank),
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .size(50.dp)
+                                    .align(Alignment.CenterEnd)
+                                    .padding(end = 16.dp)
+                            )
+                        }
+                        ExitButton(
+                            onClick = { /* Handle button click */ },
+                            icon = painterResource(id = R.drawable.cross),
+                            paddingValue = 10.dp
+                            )
+                        }
+                    }
+                }
+            }
+        }
     }
 
 
+
+
     @Composable
-    fun IconButtonWithText(
+    fun MenuBarButton(
         onClick: () -> Unit,
         icon: Painter,
-        iconSize: Dp = 24.dp
+        iconSize: Dp = 24.dp,
+        paddingValue: Dp = 0.dp
     ) {
         Button(
             onClick = onClick,
             colors = ButtonDefaults.buttonColors(
                 MenubarGray,
                 contentColor = MenubarGray
-            )
+            ), modifier = Modifier.padding(paddingValue),
+
         ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Image(
+            Image(
                     painter = icon,
                     contentDescription = null,
                     modifier = Modifier.size(iconSize)
-                )
-            }
+            )
         }
     }
+
+@Composable
+fun ExitButton(
+    onClick: () -> Unit,
+    icon: Painter,
+    iconSize: Dp = 24.dp,
+    paddingValue: Dp = 0.dp
+) {
+    Button(
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(
+            Color.White,
+            contentColor = Color.White
+        ), modifier = Modifier.padding(paddingValue)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = icon,
+                contentDescription = null,
+                modifier = Modifier.size(iconSize)
+            )
+        }
+    }
+}
 
     @Preview
     @Composable
     fun SwipeScreenPreview() {
-        SwipeScreen()
-    }
+        SwipeScreen() }
