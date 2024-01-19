5b0454bc5a1ebcaa7117d0f1df14b45754bfa45b
Nicolai <s221677@student.dtu.dk>
2024-01-19 16:30:52 +0100

    hej

--- app/src/main/java/com/example/stylebank/ui/theme/Theme.kt
+++ app/src/main/java/com/example/stylebank/ui/theme/Theme.kt
@@ -42,6 +42,8 @@ fun StyleBankTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
+
+
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
